a9a62bab5366d9a0d24f234334170f8a40440b8b
Glen Hyatt <glen.hyatt@lumi.com>
2020-02-28 13:39:32 -0500

    first

---
+++ JAVA101/Glen.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.application.Application;
+
+public class Main extends Application {
+
+
+    public void start(Stage primaryStage) throws Exception{
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+        new println("Hello From Glen");
+    }
+}
